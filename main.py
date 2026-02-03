4100e7129a3974060d2cbe58f309f3750a86a6f6
alice <alice_kuznetsov@berkeley.edu>
2026-02-03 01:08:15 -0800

    Separate terminal and windowed playback

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import cv2 as cv
 import time
+import os
+
 
 frames_to_skip = 5
 frames_per_second = 30
-processing_buffer = 0.01
+processing_buffer = 0.005
+sleep_time = (frames_to_skip/frames_per_second) - processing_buffer
 
 
 '''
@@ -22,23 +25,6 @@ TODOS:
 
 - Export Json to file
 '''
-def play_video(capture, stop_key, sleep_time, frame_spacing):
-    isTrue, frame = capture.read()
-
-    # Iterate through video frames until the given key is pressed
-    while True: 
-        status, next_frame = get_next_frame(capture, frame_spacing)
-
-        cv.imshow('Raw Video', next_frame)
-
-        time.sleep(sleep_time)
-        
-        if cv.waitKey(1) & 0xFF==ord(stop_key):
-            break
-
-    # Close the window
-    cv.destroyAllWindows()
-
 
 def get_next_frame(capture, frame_spacing):
     if frame_spacing != 0:
@@ -51,9 +37,56 @@ def get_next_frame(capture, frame_spacing):
     return capture.retrieve()
 
 
+def frame_to_string(frame):
+    image_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    
+    output = ""
+    for row in frame:
+        for px in row:
+            if (px[2] > 127):
+                output += " "
+            else:   
+                output += "0"
+        output += "\n"
+    
+    return output
+
+
+def play_video_terminal(capture, sleep_time, frame_spacing):
+    isTrue, frame = capture.read()
+
+    while True: 
+        status, next_frame = get_next_frame(capture, frame_spacing)
+
+        resized_frame = cv.resize(next_frame, (150, 150))
+
+        print(frame_to_string(resized_frame))
+        time.sleep(sleep_time)
+        os.system('cls')
+
+def play_video_windows(capture, stop_key, sleep_time, frame_spacing):
+    isTrue, frame = capture.read()
+
+    while True: 
+        status, next_frame = get_next_frame(capture, frame_spacing)
+
+        resized_frame = cv.resize(next_frame, (150, 150))
+
+        cv.imshow('Raw Video', next_frame)
+        cv.imshow('Resized Video', resized_frame)
+        
+        time.sleep(sleep_time)
+        
+        if cv.waitKey(1) & 0xFF==ord(stop_key):
+            break
+
+    # Close the window
+    cv.destroyAllWindows()
+
+
 if __name__ == "__main__":
     capture = cv.VideoCapture('bad_apple_raw.mp4')
 
-    play_video(capture, 'd', (frames_to_skip/frames_per_second) - processing_buffer, frames_to_skip)
+    play_video_terminal(capture, sleep_time, frames_to_skip)
 
     capture.release()
